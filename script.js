f71d7a829731e3c9f8f46e380f8ab90a902cf662
IngenAningVadJagSkaSkriva <123164118+IngenAningVadJagSkaSkriva@users.noreply.github.com>
2023-11-30 12:19:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 var blocker = () => {
-    window.open('window.html','','HEIGHT=1,WIDTH=1');
+    let m = window.open('','','HEIGHT=1,WIDTH=1');
+    m.document.write('<!DOCTYPE html><html><head><script src="script.js"></script><title>LANSCHOOL WORKER! DONT CLOSE!</title></head><body onclick="start()"><a id="text">CLICK ME TO START BLOCKING LANSCHOOL VIEW!</a></body></html>');
+    setInterval(() => {
+        document.write("CLOSE THIS TAB NOW!<br>");
+    }, 100);
+    close();
 }
 var x = 0;
 var start = () => {
